77d0279962829cf67bd9e044e0919addc2b9f391
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-05 23:35:02 -0300

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    let lowerBound;
+    let upperBound;
+    let sum = 0;
 
+    if (numA < 0 || 
+        numB < 0 ||
+        typeof numA != 'number' ||
+        typeof numB != 'number') {
+        return 'ERROR';
+    } else if (numA < numB) {
+        lowerBound = numA;
+        upperBound = numB;
+    } else {
+        upperBound = numA;
+        lowerBound = numB;
+    }
+
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
