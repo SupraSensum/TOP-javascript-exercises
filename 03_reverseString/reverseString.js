6587c14d733a8e6ee43d96282d4c1e7e9ce1e386
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-05 22:35:27 -0300

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(leString) {
+    let tempString = '';
+    for (let i = leString.length - 1; i >= 0; i--) {
+        tempString += leString.charAt(i);
+    }
+    return tempString;
 };
 
 // Do not edit below this line
