4a69ebf988c60b62d89063fce64044f28320a6f3
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-05 22:19:35 -0300

    Complete first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeatFactor) {
+    let affectedString = '';
+    if (repeatFactor < 0) {
+        return 'ERROR';
+    }
+    for (i = 0; i < repeatFactor; i++) {
+        affectedString += inputString;
+    }
+    return affectedString;
 };
 
 // Do not edit below this line
