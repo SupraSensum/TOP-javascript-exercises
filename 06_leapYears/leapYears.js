9b23989ca176f51bacbf05fca39f191436c7c28c
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-07 08:24:06 -0300

    Update progress on exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year = 2000) {
+    // if % 4 = 0 && % 100 != 100 || % 400 = 0
+    console.log(year % 4 === 0);
+    console.log(year % 100 != 100);
+    console.log(year % 4 === 0 && year % 100 != 100);
+    console.log(year % 400 === 0);
+    console.log((year % 4 === 0 && year % 100 != 100) || year % 400 === 0);
+    return (year % 4 === 0 && year % 100 != 100) || year % 400 === 0;
 };
 
 // Do not edit below this line
