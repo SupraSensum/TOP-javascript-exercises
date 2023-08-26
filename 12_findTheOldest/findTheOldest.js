ddc33f5c27fa3fdb8e00d4d89052b41f8fd68815
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-26 14:20:19 +0000

    Practice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+   // returns an object
+   return arr
+      .reduce((oldest, currPerson) => {
+         let currentYear = (new Date()).getFullYear();
+         let currPersonAge = 0;
+         let prevPersonAge = 0;
+         
+         // calc age of currPerson
+         if (!currPerson.yearOfDeath) { // check if currPerson still alive
+            currPersonAge = currentYear - currPerson.yearOfBirth;
+         } else {
+            currPersonAge = currPerson.yearOfDeath - currPerson.yearOfBirth;
+         }
 
+         // calc age of prevPerson
+         if (!oldest.yearOfDeath) { // check if prevPerson still alive
+            prevPersonAge = currentYear - oldest.yearOfBirth;
+         } else {
+            prevPersonAge = oldest.yearOfDeath - oldest.yearOfBirth;
+         }
+
+         return currPersonAge > prevPersonAge ? currPerson : oldest;
+      });
 };
 
 // Do not edit below this line
