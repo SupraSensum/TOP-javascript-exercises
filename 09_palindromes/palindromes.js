e64214ae697c031778bd0b1dbaf4e203f3081b80
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-26 13:23:06 +0000

    Practice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   let cleanStr = "";
 
+   // clean input
+   for (let i = 0; i < str.length; i++) {
+      const char = str[i];
+
+      if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')) {
+         cleanStr += char.toLowerCase();
+      }
+   }
+
+   for (let i = 0; i < Math.ceil(cleanStr.length / 2); i++) {
+      j = cleanStr.length - i - 1;
+      if (cleanStr[i] !== cleanStr[j]) return false;
+   }
+
+   return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// 0 1 2 3 4 5 6
+// r a c e c a r
+
+// j = length - i - 1
+// i = 0, j = 6
+// i = 6, j = 0
+
+// remove any chars that are not the alphabet
+// calculate for loop cap
+//    - Math.ceil(arr.length / 2)
+// iterate through & compare:
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+   let pre = 0,
+      curr = 1,
+      sum = 0;
+   fibIndex = Number(fibIndex);
+   
+   if (fibIndex < 0)
+      return 'OOPS';
+   else if (fibIndex === 0)
+      return 0;
+   else if (fibIndex === 1)
+      return 1;
 
+   for (let i = 2; i <= fibIndex; i++) {
+      sum = pre + curr;
+      pre = curr;
+      curr = sum;
+   }
+
+   return sum;
 };
 
 // Do not edit below this line
